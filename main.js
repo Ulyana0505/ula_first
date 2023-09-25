7b821810fa41c3dad4f364b431027c6562ffe590
Ulyana <lana.arfinova@gmail.com>
2023-09-25 18:23:24 +0300

    new two

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log(1);
+var title = document.getElementById('title');
+title.style.color = "#F5147E";
+
+var title_2 = document.getElementById('title_2');
+title_2.style.color = "#E75480";
+
+var body = document.getElementById('body');
+body.style.backgroundColor = "#FFC0CB";
+
+var whattheysaid = confirm("Красиво?");
+console.log(whattheysaid);
+
+var counter_button = 0;
+
+function onClickButton(el) {
+    counter_button++;
+    el.innerHTML = "You've clicked: " + counter_button;
+    el.style.background = "#ba55d3";
+    el.style.color = "#4b0082";
+}
