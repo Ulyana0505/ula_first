b8f3f6d22d44f375050e553e915b84bd0271eeb1
Ulyana <lana.arfinova@gmail.com>
2023-09-26 22:17:51 +0300

    new

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-setTimeout(whattheysaid, 5000);
+// setTimeout(whattheysaid, 5000);
 
-function whattheysaid() {
-    confirm("Красиво?");
-    console.log(whattheysaid);
-}
+// function whattheysaid() {
+//    confirm("Красиво?");
+//    console.log(whattheysaid);
+// }
 
 var counter_button = 0;
 
@@ -12,4 +12,8 @@ function onClickButton(el) {
     el.innerHTML = "You've clicked: " + counter_button;
     el.style.background = "#ba55d3";
     el.style.color = "#4b0082";
+}
+
+function userCalculate(a) {
+    prompt("Enter any two numbers: ");
 }
