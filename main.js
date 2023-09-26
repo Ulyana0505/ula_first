0c180d9e0f73570ffc9c36739609b941394948a9
Ulyana <lana.arfinova@gmail.com>
2023-09-26 22:08:51 +0300

    styles added

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-var title = document.getElementById('title');
-title.style.color = "#F5147E";
-
-var title_2 = document.getElementById('title_2');
-title_2.style.color = "#E75480";
-
-var body = document.getElementById('body');
-body.style.backgroundColor = "#FFC0CB";
-
 setTimeout(whattheysaid, 5000);
 
 function whattheysaid() {
